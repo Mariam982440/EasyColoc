20f9e162ec11afa8ca26142cbb172dd8720cd6f1
Mariam982440 <mariam1elhaimer@gmail.com>
2026-02-25 21:15:39 +0000

    feat : création du model category et configuration des relations

---
+++ app/Models/Category.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $fillable = [
+        'name', 
+        'colocation_id'
+    ];
+
+
+    public function expenses()
+    {
+        return $this->hasMany(Expense::class);
+    }
+}
