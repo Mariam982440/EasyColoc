318bf39d0685a01ed197c1a7efefcdccd94767a0
Mariam982440 <mariam1elhaimer@gmail.com>
2026-02-25 10:39:34 +0000

    feat : modification de la methode create pour que le premier créé soit un admin

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -26,10 +26,15 @@ class CreateNewUser implements CreatesNewUsers
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
         ])->validate();
 
+        $isFirst = User::count() === 0;
+
+        
+
         return User::create([
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
+            'role_type' => $isFirst ? 'admin' : 'user',
         ]);
-    }
+        }
 }
