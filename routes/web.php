45025e2a59d458ddaf8be602e0b9dd0feb2e5836
Mariam982440 <mariam1elhaimer@gmail.com>
2026-02-27 00:20:43 +0000

    feat : ajout des routes necessaires pour colocationColocation

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,23 @@
 <?php
 
+use App\Http\Controllers\ColocationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified',])->group(function () {
+    
+    Route::get('/dashboard',function(){
+        $user = Auth::user();
+        $colocation = $user->currentColocation();
+        return view('/dashboard',compact('colocation','user'));
+        })->name('dashboard');
+
+    Route::get('/colocation',[ColocationController::class,'index'])->name('colocations.index');
+    Route::get('/colocations/create', [ColocationController::class, 'create'])->name('colocations.create');
+    Route::post('/colocations', [ColocationController::class, 'store'])->name('colocations.store');
+    Route::get('/colocations/{colocation}', [ColocationController::class, 'show'])->name('colocations.show');
+    Route::delete('/colocations/{colocation}', [ColocationController::class, 'destroy'])->name('colocations.destroy');
 });
